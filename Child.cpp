828386a56638c3fe8fe9e704dcb20ed79c656feb
GreggH <greggory.png@gmail.com>
2019-10-30 23:52:54 -0700

    Reslease 1.0, 10/30
    Yay! Hope there's nothing wrong.

--- Child.cpp
+++ Child.cpp
@@ -1,7 +1,11 @@
-/*
+/* This program is a really crappy console application which can store the info of any number of students.
+ * You can add a student with the "ADD" command (none of these commands are case-sensitive), inputting their name, ID number, and 
+ * GPA. You can print out the list of students logged in this session with the "PRINT" command, printing them out in the order that
+ * they were logged. The "DELETE" command will delete a student by their ID number, and finally, the "QUIT" command will abort
+ * the program, deleting all students stored inside.
  *  
  * @author Greggory Hickman
- * @version dev 1.2
+ * @version release 1.0
  */
 #include <iostream>
 #include <vector>
@@ -18,6 +22,7 @@ struct Child {
 
 //Prototypes
 Child add(char* name, char* surname, int id, float gpa);
+int find(vector<Child> list, int id);
 
 int main() {
   //Input cstring
@@ -31,7 +36,7 @@ int main() {
 
   while (true) {
   
-    cout << endl << "Commands: " << endl << "ADD" << endl << "PRINT" << endl << "DELETE" << endl << "ABORT" << endl;
+    cout << endl << "Commands: " << endl << "ADD" << endl << "PRINT" << endl << "DELETE" << endl << "QUIT" << endl;
 
     cout << "> ";
 
@@ -60,12 +65,10 @@ int main() {
 	cout << "Student ID: ";
 	cin >> id;
 	//Only allow up to 6-digit id's because I believe in segregation of numbers
-	if (id <= 999999 && id > 0) {
+	if (find(list, id) == -1)
 	  valid = true;
-	}
-	else {
-	  cout << endl << "Invalid input. Please enter a 6-digit number" << endl;
-	}
+	else
+	  cout << endl << "This ID already belongs to another student. Please enter an unused 6-digit number" << endl;
       }
       
       cout << "Student GPA: ";
@@ -84,6 +87,8 @@ int main() {
 	(cmd[3] == 'N' || cmd[3] == 'n') &&
 	(cmd[4] == 'T' || cmd[4] == 't')) {
 
+      char zeroes[6];
+
       cout << endl << "-----" << endl;
 
       if (list.size() == 0) {
@@ -96,7 +101,8 @@ int main() {
 	  }
 	  cout << "Student #" << i + 1 << ":" << endl;
 	  cout << list[i].name << " " << list[i].surname << endl;
-	  cout << "Student ID: " << list[i].id << endl;
+	  cout << "Student ID: " << zeroes << list[i].id << endl;
+	  cout.precision(3);
 	  cout << "Student GPA: " << list[i].gpa << endl;
 	}
       }
@@ -111,15 +117,37 @@ int main() {
 	(cmd[3] == 'E' || cmd[3] == 'e') &&
 	(cmd[4] == 'T' || cmd[4] == 't') &&
 	(cmd[5] == 'E' || cmd[5] == 'e')) {
-      //delete a student (see vector::erase)
+
+      int id;
+
+      cout << endl << "-----" << endl;
+
+      cout << "Student ID: ";
+      cin >> id;
+      cin.ignore();
+
+      id = find(list, id); //Id is now the address of the to-be-deleted student
+      
+      if (id != -1) {
+	for (int i = id; i < list.size() - 1; i++) { //Because XCode doesn't seem to like vector.erase(id)
+	  list[i] = list[i+1];
+	}
+	list.pop_back();
+	cout << "Thank you. " << list[id].name << " " << list[id].surname  << " has been successfully located and disposed of." << endl;
+      }
+      else {
+	cout << "No student with that ID currently exists" << endl;
+      }
+      
+      cout << "-----" << endl;
+
     }
 
     //Kill the program
-    if ((cmd[0] == 'A' || cmd[0] == 'a') &&
-	(cmd[1] == 'B' || cmd[1] == 'b') &&
-	(cmd[2] == 'O' || cmd[2] == 'o') &&
-	(cmd[3] == 'R' || cmd[3] == 'r') &&
-	(cmd[4] == 'T' || cmd[4] == 't')) {
+    if ((cmd[0] == 'Q' || cmd[0] == 'q') &&
+	(cmd[1] == 'U' || cmd[1] == 'u') &&
+	(cmd[2] == 'I' || cmd[2] == 'i') &&
+	(cmd[3] == 'T' || cmd[3] == 't')) {
       cout << endl << "Okay, bye. Thank you for donating to the Child Storage Association (CSA)!" << endl;
       return 0;
     }
@@ -130,7 +158,7 @@ int main() {
 //Create a new Student, must return Child
 Child add(char* name, char* surname, int id, float gpa) {
   Child c;
-  cout.precision(2);
+  cout.precision(3);
   c.name = name;
   c.surname = surname;
   c.id = id;
@@ -139,5 +167,15 @@ Child add(char* name, char* surname, int id, float gpa) {
   cout << "Thank you for your donation." << endl;
   cout << "-----" << endl;
   return c;
+}
+
+//Return a child's iterator address in the list vector with the ID inputted.
+int find(vector<Child> list, int id) {
+  for (int i = 0; i < list.size(); i++) {
+    if (list[i].id == id) {
+      return i;
+    }
+  }
+  return -1; //This means that there are no students with the inputted id
 }
 
